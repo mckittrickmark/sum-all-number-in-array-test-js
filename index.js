7f78503258f0b1c9daa837def3ec7325184d8889
Mark Mckittrick <mckittrickmark@gmail.com>
2018-10-23 19:10:44 +0000

    excercise complete

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+let total = 0
+let n = 0
+
 function sumItems(array) {
-  // Sum all the numbers in the array
+  total = 0
+  for (var item of array) {
+    if (Array.isArray(item)){
+      total += sumItems(item)
+    } else {
+      total += item
+    }
+  }
+  return total
 }
+
 
 module.exports = sumItems;
